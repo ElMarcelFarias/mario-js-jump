835e57959e8e4811e28dc10d9dca7b9f4aa18f0a
Marcel Leite de Farias <marcel.leitefarias@gmail.com>
2022-05-31 10:30:47 -0300

    Finalizando parte lógica do jogo!

--- js/script.js
+++ js/script.js
@@ -13,8 +13,24 @@ const jump = () => {
 
 const loop = setInterval(()=>{
 
-    const pipePosition = pipe.offsetLeft;
-    
+    const pipe_position = pipe.offsetLeft;
+    const mario_position = +window.getComputedStyle(mario).bottom.replace('px', '');
+
+    if(pipe_position <= 120 && pipe_position > 0 && mario_position < 80) {
+
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipe_position}px`;
+
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${mario_position}px`;
+
+        mario.src = 'images/game-over.png';
+        mario.style.width = '75px';
+        mario.style.marginLeft = '50px';
+
+        clearInterval(loop);
+    }
 
 },10)
 
