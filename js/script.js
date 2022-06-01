362a8ef55390db56ea8b8249a12ca347628365f2
Marcel Leite de Farias <marcel.leitefarias@gmail.com>
2022-06-01 15:33:45 -0300

    Implementando coisas novas ao projeto!

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
+const numero_de_pulos = 12;
 
 const jump = () => {
     mario.classList.add('jump-mario');
@@ -11,16 +12,18 @@ const jump = () => {
     },500)
 }
 
+
 const loop = setInterval(()=>{
 
     const pipe_position = pipe.offsetLeft;
     const mario_position = +window.getComputedStyle(mario).bottom.replace('px', '');
-
+    alert(numero_de_pulos)
+    
     if(pipe_position <= 120 && pipe_position > 0 && mario_position < 80) {
 
         pipe.style.animation = 'none';
         pipe.style.left = `${pipe_position}px`;
-
+        
 
         mario.style.animation = 'none';
         mario.style.bottom = `${mario_position}px`;
@@ -29,6 +32,7 @@ const loop = setInterval(()=>{
         mario.style.width = '75px';
         mario.style.marginLeft = '50px';
 
+       
         clearInterval(loop);
     }
 
